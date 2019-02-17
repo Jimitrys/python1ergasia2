ea1161075531b0cc6fd4f5f4fd8e71819edfd939
Jimitrys <45967385+Jimitrys@users.noreply.github.com>
2019-02-17 16:18:07 +0200

    Update ergasia3.py

--- ergasia3.py
+++ ergasia3.py
@@ -1,13 +1,14 @@
 # -*-coding:utf-8-*-
 import sys
 import os
-#για επανάληψη του προγράμματος
+#για επανάληψη
 play = True
 while play == True:
     
     def main():
         f = raw_input("Δώστε το όνομα του αρχείου: ")
         if os.path.isfile(f):
+            #ανοιγμα αρχείου και διαβασμα γραμμων
             infile = open(f, "r")
             outfile = open("newfile.txt", "w")
             UNI = raw_input("Περιλαμβάνει unicode χαρακτήρες το αρχείο σας; Y/N ").capitalize()
@@ -16,10 +17,30 @@ while play == True:
                 outfile.write(infile.readline())
             else:
                 pass
-            for line in infile:
-                #για κάθε γραμμή κειμένου ελέγχει αν ξεκινάει απο #
-                #αφου πρώτα αγνοεί τα κενά
-                if not line.lstrip().startswith("#"):
+
+            lines=infile.readlines()
+
+            for line in lines:
+                #αν περιεχει #
+                if "#" in line:
+                    l=line.strip()
+                    #για κάθε γραμμή κειμένου ελέγχει αν ξεκινάει απο #
+                    #αφου πρώτα αγνοεί τα κενά
+                    if not l.startswith("#"):
+                        newline = line.split("#")[0]
+                        no = "\n"
+                        #μετραει τα αφτιά
+                        a1=newline.count("'")
+                        a2=newline.count('"')
+                        #αν ειναι μονο το πληθος το # ειναι μεσα σε αλφαριθμητικο
+                        if (a1%2)==1 or a2%2==1:
+                        	outfile.write(line)
+                        else:
+                            #εκτυπώνει στο νέο αρχείο την σειρά χωρίς κενό και μια εξτρα κενή σειρά (no)
+                            #για να εχει ακριβώς την ίδια μορφή με το αρχικό
+                            outfile.write(newline)
+                            outfile.write(no)
+                else:
                     outfile.write(line)
             infile.close()
             outfile.close()
@@ -29,9 +50,7 @@ while play == True:
             print "Τα σχόλια διαγράφηκαν επιτυχώς!"
         else:
             print "Το αρχείο που δώσατε δεν υπάρχει."
-            
     main()
-    #για επανάληψη η παύση του προγράμματος
     stop = raw_input("Πατήστε R για επανάληψη ή X για έξοδο: ").capitalize()
     if stop == "R":
         play = True
